30e997007e371c410b46c6ab3db197f40388a673
Barak <barakc121@gmail.com>
2025-12-25 12:43:02 +0200

    a

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from 'express'
-import usersRoutes from './routes/usersRoutes.js';
-import events from './routes/events.js';
-const app = express()
+import express from "express";
+import usersRoutes from "./routes/usersRoutes.js";
+import events from "./routes/events.js";
+const app = express();
 
 app.use(express.json());
 
-app.use('/users', usersRoutes);
-app.use('/creator', events);
+app.use("/tickets",tickets)
+app.use("/users", usersRoutes);
+app.use("/creator", events);
 
-app.listen(3000, () => console.log('listening'));
+app.listen(3000, () => console.log("listening"));
