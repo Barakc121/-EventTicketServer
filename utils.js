2c44eb58d0fb19ddbb541490d5ab7546a586f1ac
Barak <barakc121@gmail.com>
2025-12-25 12:56:25 +0200

    d

--- utils.js
+++ utils.js
@@ -24,25 +24,6 @@ export async function create(data, key, unique) {
 }
 
 
-export async function create(data, key, unique) {
-  try {
-    const collection = await read(key + ".json") || [];
-    data = { id: collection[collection.length - 1]?.id + 1 || 1, ...data };
-    if (collection.some(item => item[unique] === data[user])) {
-    if (collection.some(item => item[unique] === data[event])) {
-      throw new Error(`The ${key.slice(0,key.length-2)} is already exists`);
-    }} else {
-      collection.push(data);
-      console.log("message:User registered successfully");
-      events.json[quantity]-receipts.json[quantity]
-      await fs.writeFile(key + ".json", JSON.stringify(collection, null, 2));
-      return data, {"message": "Tickets purchased successfully" };
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 
 
 
